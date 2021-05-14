1efa4f2bfec6ecfbabcd715f4bfb39d2e0728cf7
Ulada <70239279+wadewilsones@users.noreply.github.com>
2021-05-14 12:45:52 -0700

    Update task-1.js

--- task-1.js
+++ task-1.js
@@ -5,3 +5,11 @@ let flag = true;
 let txt = "true";
 
 console.log (typeof num)
+
+// Volume
+let heightC = 10;
+let dC = 10;
+let r = dC/2;
+let sC = 3.14*(Math.pow(r,2));
+let V = heightC*sC;
+console.log (V)
