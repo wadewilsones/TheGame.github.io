27c8c687d9e858949b6d994e5ff8af0c3151207f
Ulada <70239279+wadewilsones@users.noreply.github.com>
2021-05-15 12:51:15 -0700

    Update rockGames.js
    Added variable for computer's choice

--- rockGames.js
+++ rockGames.js
@@ -2,4 +2,27 @@ let paper = document.getElementById('Paper');
 let rock = document.getElementById('Rock');
 let scissors = document.getElementById('Scissors');
 let subbutton = document.getElementById('submit');
+
 subbutton.addEventListener ('onclick', function Start(){console.log ("Im working!")})
+
+
+//Declare choice for computer
+
+let compChoice =  Math.floor(Math.random() * 3);
+
+    if (compChoice === 0) {
+        compChioce = paper; 
+        console.log ("Copmuter Choose paper");
+    };
+
+    if (compChoice === 1) {
+        compChioce = rock; 
+        console.log ("Copmuter Choose rock")
+    };
+
+    if (compChoice === 2) {
+        compChioce = scissors; 
+        console.log ("Copmuter Choose scissors")
+    };
+console.log (compChoice)
+})
