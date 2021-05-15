658caa824770309db4ca4769f054ef3bff2a00db
Ulada <70239279+wadewilsones@users.noreply.github.com>
2021-05-15 11:55:36 -0700

    Update rockGames.js

--- rockGames.js
+++ rockGames.js
@@ -1,4 +1,5 @@
-    let rock = 'rock';
-    let paper = 'paper';
-    let scissors = 'scissors';
-
+let paper = document.getElementById('Paper');
+let rock = document.getElementById('Rock');
+let scissors = document.getElementById('Scissors');
+let subbutton = document.getElementById('submit');
+subbutton.addEventListener ('onclick', function Start(){console.log ("Im working!")})
