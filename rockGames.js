0c988c05dcf47225bb40e6d3d87742c7a485e3f8
Ulada <70239279+wadewilsones@users.noreply.github.com>
2021-05-17 18:14:05 -0700

    Update rockGames.js

--- rockGames.js
+++ rockGames.js
@@ -1,76 +1,90 @@
+// Declare variables for DOM elements
+
+let paper = document.getElementById('paper');
+let rock = document.getElementById('rock');
+let scissors = document.getElementById('scissors');
+
 let subbutton = document.getElementById('submitB');
-let choise = document.getElementById('choise');
+
+//let choise = document.getElementById('choise'); input text remove
+
+//Click  
 
 subbutton.addEventListener ('click', function Start(){
 
 //Declare choice for computer.
 
-let compChoice =  Math.floor(Math.random() * 3)
-    if (compChoice === 0) {
+    let compChoice =  Math.floor(Math.random() * 3)
+
+        if (compChoice === 0) {
         compChoice = "paper"; 
-       
-    };
-    if (compChoice === 1) {
+        };
+        if (compChoice === 1) {
         compChoice = "rock"; 
-       
-    };
-    if (compChoice === 2) {
-        compChoice = "scissors"; 
-       
-    };
+        };
+        if (compChoice === 2) {
+        compChoice = "scissors";    
+        };
 
-validation();
-console.log (`Computer choose: ${compChoice}`);
-console.log (`User choose: ${choise.value}`);
-console.log ("Result is " + compare(compChoice, choise.value))
+    console.log (`Computer choose: ${compChoice}`);
+    xMan();
+    let getUserChoice = xMan;
+    console.log ("Result is: " + user);
+    })
 
 
-})
+// Get user choise
 
-// Input validation
-
-function validation() {
-    if (choise.value === "paper" || choise.value  === "rock" || choise.value  === "scissors")
-    {console.log ("I'm working! and input is " + choise.value)}
-    else {console.log("I'm not working")}
+    function xMan() {
     
+    if(paper.checked) 
+    {   user = "paper";
+        console.log ("User choose: paper")}
+    if(rock.checked)
+    {   user = "rock";
+        console.log ("User choose: rock")}
+    if(scissors.checked)
+    {   user = "scissors";
+        console.log("User choose: scissors")}
+   
 }
+
+
+
 
 // Compare user and computer choise
 
 function compare(computer, human) {
 
-    if (computer === human) {
-
-        console.log ("It's a tie")
+    if (computer === human) 
+    {    
         return "It's a tie"
     }
 
     if (computer === "paper")
         {
             if(human === "rock")
-            {console.log ("user won");
+            {
             return "user won"}
-            else {console.log ("user lost")
+            else {
             return "user lost"}
-        }
+    }
     if (computer === "paper")
         {
             if(human === "scissors")
-            {console.log("User won")
+            {
             return "user won"}
-            else {console.log ("user lost")
+            else {
             return "user lost"}
-        }
+    }
     if (computer === "scissors") {
 
-            if (human === "rock"){console.log("User won")
+            if (human === "rock"){
             return "user won"}
-            else {console.log ("user lost")
+            else {
             return "user lost"}
 
-            }
-
-
+    }
 
 }
+
