3ea96ab59964fb0a6d6827853776c2c5fa96e78a
Ulada <70239279+wadewilsones@users.noreply.github.com>
2021-05-22 20:42:40 -0700

    Update rockGames.js

--- rockGames.js
+++ rockGames.js
@@ -1,126 +1,117 @@
-//Connection with HTML elements
-const zipCode = document.getElementById ('zip');
-const generate = document.getElementById ('generate');
-const feelings = document.getElementById ('feelings');
-const place = document.getElementById ('location');
-const date = document.getElementById('date');
-const temp = document.getElementById('temp');
-const content = document.getElementById('content');
+// Name script 
+
+let name = document.getElementById('name_input');
+let sub_btn = document.getElementById("name_sbt");
+let greeting = document.getElementById("greeting");
+let greet = document.getElementById("nameCheck");
+
+let sectionOne = document.querySelector(".name_confirmation_section");
+let sectionTwo = document.querySelector(".secondScreen");
 
 
-
-// Base URL for OpenWeatherMap API
-  const baseURL = 'http://api.openweathermap.org/data/2.5/weather?zip=';
-  const apiKey = 'f160bf0abe9fe5dde87e073cda69e092';
-
-//Retrive Weather Data from Openweather API
-
-const getWeatherData = async (Zipcode) => {
-  const response = await fetch (baseURL+Zipcode+',US'+'&appid='+apiKey+'&units=metric')
-  try {
-    const data = await response.json();
-    return data;
-  }
-  catch (error) {console.log ("error",error);}
-  console.log (response)
-
-} 
-
-//Async Post
-
-const postData = async (url ='', data = {}) => {
-  const res = await fetch (url ='http://localhost:3000/add', {
-    method: 'POST',
-    credentials: 'same-origin',
-    headers: {
-        'Accept': 'application/json',
-        'Content-type': 'application/json'
-    },
-    body: JSON.stringify(data),
-  })
-  try {
-    const newData = await res.json();
-    return newData;
-  }
-  catch (error) {console.log ("error",error);}
-  }
-
-
-//Async Get
-
-
-
-
-
-
-// Post data
-generate.addEventListener ('click', addZipCode)
-async function addZipCode (e) {
-
-  if(zip.value != "" && feelings.value != ""){
-    let currentDate = getnewDate();//new Date().toDateString();
-    let data = await getWeatherData(zipCode.value);
-    let tempValue = Math.round(data.main.temp);
-    let locationValue = data.name + ", " + data.sys.country ;
-    let weatherValue = data.weather[0].main +' '+ Math.round(data.main.feels_like);
-    let contentValue = document.getElementById ('feelings').value
-    
-    postData('/add', {
-        date: currentDate,
-        location: locationValue,
-        temp: tempValue,
-        weather: weatherValue,
-        content: contentValue
-    })
-    .then (UI())
-    .then (cleanUI());
-}
-
-else 
-{
-  if (zip.value == "" && feelings.value == "") 
-  {
-    zipCode.value = "Enter a correct zip-code!";
-    feelings.value = "Enter  a correct feeling!";
-  }
-  if (zip.value != "" && feelings.value == "") 
-  {
-    feelings.value = "Enter a correct feeling!";
-  }
-  if (zip.value == "" && feelings.value != "") 
-  {
-
-    zipCode.value = "Enter a correct zip-code!";
-  }
-
-}
-
-}
-/* Function to GET Project Data */
-const UI = async () => {
-  const updateData = await fetch ('/add'); // жди прихода от апи
-    const allData = await updateData.json();
-    try{
-   
-    temp.innerHTML = allData[allData.length - 1].temp + '°';
-    content.innerHTML = 'And today I feel '+ allData[allData.length - 1].content;
-    place.innerHTML = allData[allData.length - 1].location;
-    date.innerHTML = getnewDate();//allData[allData.length - 1].date;
+sub_btn.addEventListener('click', function(event){
+    event.preventDefault()
+    if (name.value !="") 
+    {
+    greeting.innerHTML = "Alright, let's play " + name.value + "!"; 
+    greet.innerHTML = "Please, choose one of the following:"
+    sectionTwo.removeAttribute('class','secondScreen');
+    sectionOne.removeAttribute('class','name_confirmation_section');
+    sectionOne.setAttribute('class','name_confirmation_section_remove');
     }
-    catch (error) {console.log ('error', error)}
-}
-function cleanUI(){
-  zipCode.value = "";
-  feelings.value= "";
+    else {
+    greet.innerHTML = "Aww, you need to type your name, dear!".bold();
+    }
+})
+
+
+
+
+// Declare variables for DOM elements
+
+let paper = document.getElementById('paper');
+let rock = document.getElementById('rock');
+let scissors = document.getElementById('scissors');
+let subbutton = document.getElementById('submitB');
+let results = document.getElementById ("results");
+
+subbutton.addEventListener ('click', function Start(){
+
+//Create random choise option for the computer.
+
+    let compChoice =  Math.floor(Math.random() * 3)
+
+        if (compChoice === 0) {
+        compChoice = "paper"; 
+        };
+        if (compChoice === 1) {
+        compChoice = "rock"; 
+        };
+        if (compChoice === 2) {
+        compChoice = "scissors";    
+        };
+
+    console.log (`Computer choose: ${compChoice}`);
+    xMan();
+    let getUserChoice = xMan;
+    let newresults = compare(compChoice, user);
+    console.log("Result is " + compare(compChoice, user))
+    results.innerHTML = compare(compChoice, user);
+        
+    })
+
+
+//Create function for human choise.
+
+    function xMan() {
+    
+    if(paper.checked) 
+    {   user = "paper";
+        console.log ("User choose: paper")}
+    if(rock.checked)
+    {   user = "rock";
+        console.log ("User choose: rock")}
+    if(scissors.checked)
+    {   user = "scissors";
+        console.log("User choose: scissors")}
+   
 }
 
-const getnewDate = () => {
-let fullDate = new Date(); 
-let data = fullDate.getDate();
-let year = fullDate.getFullYear();
-let month = ['Jan', 'Feb', 'March', 'April', 'May', 'June', 'July', 'August', 'Sep', 'Oct', 'Nov', 'Dec']
-return (month[fullDate.getMonth()]+' ' + data + ', '+ year);
-console.log (month[fullDate.getMonth()]+' ' + data + ', '+ year)
+// Compare function for  user and computer choise
+
+function compare(computer, human) {
+
+    if (computer === human) 
+    {    
+        return "It's a tie"
+    }
+
+    if (computer === "rock")
+        {
+            if(human === "paper")
+            {
+            return "user won"}
+            else {
+            return "user lost"}
+    }
+    if (computer === "paper")
+        {
+            if(human === "scissors")
+            {
+            return "user won"}
+            else {
+            return "user lost"}
+    }
+    if (computer === "scissors") {
+
+            if (human === "rock"){
+            return "user won"}
+            else {
+            return "user lost"}
+
+    }
+
 }
 
-getnewDate();
+
+
