de856bdb7a03e51c68d671dbe07ba2e83e62d58f
Ulada <70239279+wadewilsones@users.noreply.github.com>
2021-05-26 17:29:19 -0700

    Update rockGames.js
    1. Submit button value were changed
    2. Fixed result padding.
    3. Hover removed

--- rockGames.js
+++ rockGames.js
@@ -3,8 +3,10 @@
 let name = document.getElementById('name_input');
 let sub_btn = document.getElementById("name_sbt");
 let greeting = document.getElementById("greeting");
-let greet = document.getElementById("nameCheck");
+let nameCheck = document.getElementById("nameCheck");
 
+let imgConsole = document.querySelector(".console_pic");
+let header = document.querySelector("h1");
 let sectionOne = document.querySelector(".name_confirmation_section");
 let sectionTwo = document.querySelector(".secondScreen");
 
@@ -14,18 +16,22 @@ sub_btn.addEventListener('click', function(event){
     if (name.value !="") 
     {
     greeting.innerHTML = "Alright, let's play " + name.value + "!"; 
-    greet.innerHTML = "Please, choose one of the following:"
+    greeting.style.cssText = "padding-top:20%" 
+    greeting.style.pointerEvents = "none"; // Removed hover pseudoclass
+    nameCheck.innerHTML = "Please, choose one of the following:";
+
+    imgConsole.removeAttribute('class','console_pic');
+    imgConsole.setAttribute('class','console_pic_remove');
+
     sectionTwo.removeAttribute('class','secondScreen');
+    sectionTwo.setAttribute('class','secondScreenDisplay');
     sectionOne.removeAttribute('class','name_confirmation_section');
     sectionOne.setAttribute('class','name_confirmation_section_remove');
     }
     else {
-    greet.innerHTML = "Aww, you need to type your name, dear!".bold();
+    nameCheck.innerHTML = "Aww, you need to type your name, dear!".bold();
     }
 })
-
-
-
 
 // Declare variables for DOM elements
 
@@ -53,11 +59,16 @@ subbutton.addEventListener ('click', function Start(){
 
     console.log (`Computer choose: ${compChoice}`);
     xMan();
+
     let getUserChoice = xMan;
     let newresults = compare(compChoice, user);
-    console.log("Result is " + compare(compChoice, user))
+
+    console.log("Result is " + compare(compChoice, user));
+    subbutton.value = "Try again!"
+    header.style.cssText = "display:none";
+    nameCheck.style.cssText = "display:none";    
     results.innerHTML = compare(compChoice, user);
-        
+    results.style.cssText = "font-size:64px; text-align:center; padding-top:20%; padding-bottom:2%";    
     })
 
 
@@ -83,31 +94,32 @@ function compare(computer, human) {
 
     if (computer === human) 
     {    
-        return "It's a tie"
+        let tie = "tie!"
+        return "It's a " + tie
     }
 
     if (computer === "rock")
         {
             if(human === "paper")
             {
-            return "user won"}
+            return "Aww, you won!"}
             else {
-            return "user lost"}
+            return "Hooray, I won!"}
     }
     if (computer === "paper")
         {
             if(human === "scissors")
             {
-            return "user won"}
+                return "Aww, you won!"}
             else {
-            return "user lost"}
+                return "Hooray, I won!"}
     }
     if (computer === "scissors") {
 
             if (human === "rock"){
-            return "user won"}
+                return "Aww, you won!"}
             else {
-            return "user lost"}
+                return "Hooray, I won!"}
 
     }
 
